14a717107546795a97e310b3724ff26a6e14c254
btaps <btapiasierra@outlook.com>
2023-04-05 00:04:27 -0700

    Copied example over from mrdoob

--- main.js
+++ main.js
@@ -1,33 +1,139 @@
 import * as THREE from 'three'
 
-// Grab canvas
-const canvas = document.querySelector('canvas')
+let camera, scene, renderer;
+let plane;
+let pointer, raycaster, isShiftDown = false
 
-// Renderer
-const renderer = new THREE.WebGLRenderer({antialias: true, canvas})
+let rollOverMesh, rollOverMaterial
+let cubeGeo, cubeMaterial
 
-const canvasWidth = renderer.domElement.clientWidth
-const canvasHeight = renderer.domElement.clientHeight
+const objects = []
 
-// Scene
-const scene = new THREE.Scene()
+init()
+render()
 
-// Camera
-const camera = new THREE.PerspectiveCamera(45, canvasWidth / canvasHeight, 1, 500)
-camera.position.set(0, 0, 100)
-camera.lookAt(0, 0, 0)
+function init() {
+	// Camera
+	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000)
+	camera.position.set(500, 800, 1300)
+	camera.lookAt(0, 0, 0)
+	
+	// Scene
+	scene = new THREE.Scene()
+	scene.background = new THREE.Color(0xf0f0f0)
+	
+	// Roll-over helpers
+	const rollOverGeo = new THREE.BoxGeometry(50, 50, 50)
+	rollOverMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, opacity: 0.5, transparent: true})
+	rollOverMesh = new THREE.Mesh(rollOverGeo, rollOverMaterial)
+	scene.add(rollOverMesh)
 
-const material = new THREE.LineBasicMaterial({color: 0x0000ff})
+	// Cubes
+	cubeGeo = new THREE.BoxGeometry(50, 50, 50)
+	cubeMaterial = new THREE.MeshLambertMaterial({color: 0xfeb74c, map: new THREE.TextureLoader().load('textures/square-outline-textured.png')})
 
-const points = []
-points.push(new THREE.Vector3(-10, 0, 0))
-points.push(new THREE.Vector3(0, 10, 0))
-points.push(new THREE.Vector3(10, 0, 0))
+	// Grid
+	const gridHelper = new THREE.GridHelper(1000, 20)
+	scene.add(gridHelper)
 
-const geometry = new THREE.BufferGeometry().setFromPoints(points)
+	// Raycaster
+	raycaster = new THREE.Raycaster()
+	pointer = new THREE.Vector2()
 
-const line = new THREE.Line(geometry, material)
+	const geometry = new THREE.PlaneGeometry(1000, 1000)
+	geometry.rotateX( - Math.PI / 2)
 
-scene.add(line)
+	plane = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({visible: false}))
+	scene.add(plane)
 
-renderer.render(scene, camera)
+	objects.push(plane)
+
+	// Lights
+	const ambientLight = new THREE.AmbientLight(0x606060)
+	scene.add(ambientLight)
+
+	const directionalLight = new THREE.DirectionalLight(0xffffff)
+	directionalLight.position.set(1, 0.75, 0.5).normalize()
+	scene.add(directionalLight)
+
+	renderer = new THREE.WebGLRenderer({antialias: true})
+	renderer.setPixelRatio(window.devicePixelRatio)
+	renderer.setSize(window.innerWidth , window.innerHeight)
+
+	document.body.appendChild(renderer.domElement)
+
+	document.addEventListener('pointermove', onPointerMove)
+	document.addEventListener('pointerdown', onPointerDown)
+	document.addEventListener('keydown', onKeyDown)
+	document.addEventListener('keyup', onKeyUp)
+
+	document.addEventListener('resize', onWindowResize)
+}
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	render()
+}
+
+function onPointerMove(event) {
+	pointer.set((event.clientX / window.innerWidth) * 2 - 1, - (event.clientY / window.innerHeight) * 2 + 1)
+	raycaster.setFromCamera(pointer, camera)
+
+	const intersects = raycaster.intersectObjects( objects, false );
+
+	if (intersects.length > 0) {
+		const intersect = intersects[0]
+		rollOverMesh.position.copy(intersect.point).add(intersect.face.normal)
+		rollOverMesh.position.divideScalar(50).floor().multiplyScalar(50).addScalar(25)
+		render()
+	}
+}
+
+function onPointerDown( event ) {
+
+	pointer.set((event.clientX / window.innerWidth) * 2 - 1, - (event.clientY / window.innerHeight) * 2 + 1);
+
+	raycaster.setFromCamera(pointer, camera);
+
+	const intersects = raycaster.intersectObjects(objects, false);
+
+	if (intersects.length > 0) {
+		const intersect = intersects[ 0 ];
+		// delete cube
+		if (isShiftDown) {
+			if (intersect.object !== plane) {
+				scene.remove(intersect.object);
+				objects.splice(objects.indexOf( intersect.object), 1);
+			}
+		// create cube
+		} else {
+			const voxel = new THREE.Mesh(cubeGeo, cubeMaterial);
+			voxel.position.copy(intersect.point).add(intersect.face.normal);
+			voxel.position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
+			scene.add(voxel);
+			objects.push(voxel);
+		}
+		render();
+	}
+}
+
+function onKeyDown( event ) {
+	switch (event.keyCode) {
+		case 16: isShiftDown = true
+		break
+	}
+}
+
+function onKeyUp( event ) {
+	switch (event.keyCode) {
+		case 16: isShiftDown = false
+		break
+	}
+}
+
+function render() {
+	renderer.render(scene, camera)
+}
